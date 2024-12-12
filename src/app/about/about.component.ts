4e49df6ea72f60aedb8391944801c41044b76a44
Haçik Ulu <hacikd2@gmail.com>
2024-12-12 20:47:51 +0300

    Intro to Angular Projects

---
+++ src/app/about/about.component.ts
@@ -0,0 +1,15 @@
+import { Component, ViewEncapsulation } from "@angular/core";
+
+@Component({
+    standalone:true,
+    selector:"app-about",
+    templateUrl:'./about.component.html',
+    styleUrl:"./about.component.css",
+    imports:[],
+    providers:[],
+    encapsulation:ViewEncapsulation.None
+}) 
+
+export class AboutComponent{
+
+}
---
+++ src/app/contact/contact.component.ts
@@ -0,0 +1,10 @@
+import { Component, isStandalone } from "@angular/core";
+
+@Component({
+    standalone:true,
+    template:"<h1 style='background-color: blue;'>Contact Page</h1>",
+    selector:"app-contact"
+})
+export class ContactComponent{
+    
+}
