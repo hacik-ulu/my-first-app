681db5a705edd2d95ed1935e00e2b7b603dbb2e7
Haçik Ulu <hacikd2@gmail.com>
2024-12-13 14:31:59 +0300

    Data Bindings and other rules have worked.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,34 @@ import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,HomeComponent,AboutComponent,ContactComponent],
+  imports: [RouterOutlet,HomeComponent,AboutComponent,ContactComponent,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
-  title = 'my-first-app';
+  helloWorld:string ="Hello, world from TS !";
+  pClassName:string="red";
+
+  // class içindeki Method olur, class dışındaki fonksiyondur.
+
+  clickMe()
+  {
+      alert("Bu butona basarak clickMe fonksiyonuu tetiklediniz.")
+  }
+
+  changeHelloWorldVariable(event:any)
+  {
+    this.helloWorld = event.target.value;
+  }
+
+  setPElementClass()
+  {
+      return "red"
+  }
 }
+
